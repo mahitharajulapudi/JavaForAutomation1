42bfe41c0887ddf673a9be4bcb32ae5a65a9e146
mr <u115595@gmail.com>
2022-01-25 21:55:02 -0600

    Data types

---
+++ JavaForAutomation/src/com/nextgen/programmingfundamentals/DataTypeProgram.java
@@ -0,0 +1,68 @@
+package com.nextgen.programmingfundamentals;
+/*==============================================================================
+ * | Author :  Mahitha Rajulapudi
+ * | Project:  NxtGen Automation
+ * | Purpose:  This java program is used to get the user input
+ * | Date   :  09/20/2021
+ * *===========================================================================*/
+
+public class DataTypeProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//This is used to display welcome message
+		System.out.println("Welcome to second day Java Cass");
+		/*Multiline comment
+		 *This program is
+		 *used to welcome the
+		 *user 	
+		 */
+		
+		//byte data type	
+		byte month = 11;
+		System.out.println("Byte data type value is " + month);	
+		
+		//short data type
+		short quantity = 1000;	
+		System.out.println("Short data type vaue is " + quantity);	
+
+		//integer data type
+		int empNumber = 6789;
+		System.out.println("Integer data type value is " + empNumber);		
+
+		//Long data type
+		long profit = 2389654987123L;
+		System.out.println("Long data type value is " + profit);
+
+		//float data type
+		float pieValue = (float)5.9876543212345;
+		System.out.println("float data type value is " + pieValue);
+
+		//float data type
+		float pieValue2 = 5.9876543212345f;
+		System.out.println("float data type value is " + pieValue2);
+
+		//double data type
+		double pieValue3 = 5.9876543212345;
+		System.out.println("Double data type value is " + pieValue3);
+
+		//Boolean data type
+		boolean status = true;
+		System.out.println("Boolean data type value is " + status);
+
+		//character data type
+		char initial = 'M';
+		System.out.println("character data type value is " + initial);
+
+		// String data type
+		String firstName = "Mahitha";
+		System.out.println("String data type value is " + firstName);
+
+
+
+
+
+
+	}
+
+}
